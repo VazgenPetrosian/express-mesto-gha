f68e0bc15e30a34de8c31b0aca242a1ec10f17a7
Vazgen Petrosian <ekaterina.vanshet@MacBook-Air-Ekaterina-2.local>
2023-08-27 20:09:30 +0300

    Tryy

--- controllers/card.js
+++ controllers/card.js
@@ -1,11 +1,6 @@
 const mongoose = require('mongoose');
-const {
-  HTTP_STATUS_BAD_REQUEST,
-  HTTP_STATUS_INTERNAL_SERVER_ERROR,
-  HTTP_STATUS_CREATED,
-  HTTP_STATUS_NOT_FOUND,
-  HTTP_STATUS_OK,
-} = require('http2').constants;
+const { HTTP_STATUS_BAD_REQUEST, HTTP_STATUS_INTERNAL_SERVER_ERROR } =
+  require('http2').constants;
 const cardModel = require('../models/card');
 
 const createCard = (req, res) => {
